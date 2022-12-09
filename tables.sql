c360070a5261bc33b4133cb44f8ee7322866f115
Adam Biśta <adamosekk2002@gmail.com>
2022-12-09 23:38:25 +0100

    added constraint 3

--- tables.sql
+++ tables.sql
@@ -114,8 +114,7 @@ CREATE TABLE Products (
     UnitPrice int  NOT NULL DEFAULT 0,
 	CONSTRAINT properPrices CHECK ((UnitPrice >= 0)),
 	CONSTRAINT ProductID_Products CHECK (Products.ProductID LIKE '^\d*$'),
-    CONSTRAINT Products_pk PRIMARY KEY  (ProductID),
-	
+    CONSTRAINT Products_pk PRIMARY KEY  (ProductID)
 );
 
 CREATE TABLE ProductPrices (
