41b445605adec358a4215a8f203d44a785e91cdd
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2022-12-09 23:35:00 +0100

    update tables

--- tables.sql
+++ tables.sql
@@ -153,8 +153,8 @@ CREATE TABLE Products (
     ProductName varchar(64)  NOT NULL,
     CategoryID char(10)  NOT NULL,
     UnitPrice int  NOT NULL DEFAULT 0,
-	  CONSTRAINT properPrices CHECK ((UnitPrice >= 0)),
-	  CONSTRAINT ProductID_Products CHECK (Products.ProductID LIKE '^\d*$'),
+    CONSTRAINT properPrices CHECK ((UnitPrice >= 0)),
+    CONSTRAINT ProductID_Products CHECK (Products.ProductID LIKE '^\d*$'),
     CONSTRAINT Products_pk PRIMARY KEY  (ProductID),
 	
 );
