1f988cfe3fe6be7f545d82c8d2a58ba64c081857
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-11 17:36:07 +0100

    fix formatting

--- tables.sql
+++ tables.sql
@@ -77,260 +77,299 @@ CREATE TABLE DiningTables
     CONSTRAINT DiningTables_pk PRIMARY KEY (DiningTableID)
 );
 
-CREATE TABLE Invoices (
-    InvoiceID char(10)  NOT NULL,
-    OrderID char(10)  NOT NULL,
+CREATE TABLE Invoices
+(
+    InvoiceID char(10) NOT NULL,
+    OrderID   char(10) NOT NULL,
     CONSTRAINT InvoiceID_Invoices_c CHECK (InvoiceID LIKE '^\d*$'),
     CONSTRAINT OrderID_Invoices_c CHECK (OrderID LIKE '^\d*$'),
-    CONSTRAINT Invoices_pk PRIMARY KEY  (InvoiceID)
+    CONSTRAINT Invoices_pk PRIMARY KEY (InvoiceID)
 );
 
-CREATE TABLE Menu (
-    MenuID char(10)  NOT NULL,
-    MenuName varchar(64)  NOT NULL,
-    FromTime datetime  NOT NULL,
-    ToTime datetime  NULL,
+CREATE TABLE Menu
+(
+    MenuID   char(10)    NOT NULL,
+    MenuName varchar(64) NOT NULL,
+    FromTime datetime    NOT NULL,
+    ToTime   datetime    NULL,
     CONSTRAINT MenuID_Menu_c CHECK (MenuID LIKE '^\d*$'),
     CONSTRAINT Proper_Dates_Menu_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
-    CONSTRAINT Menu_pk PRIMARY KEY  (MenuID)
+    CONSTRAINT Menu_pk PRIMARY KEY (MenuID)
 );
 
-CREATE TABLE MenuDetails (
-    MenuID char(10)  NOT NULL,
-    ProductID char(10)  NOT NULL,
+CREATE TABLE MenuDetails
+(
+    MenuID    char(10) NOT NULL,
+    ProductID char(10) NOT NULL,
     CONSTRAINT MenuID_MenuDetails_c CHECK (MenuID LIKE '^\d*$'),
     CONSTRAINT ProductID_MenuDetails_c CHECK (ProductID LIKE '^\d*$'),
-    CONSTRAINT MenuDetails_pk PRIMARY KEY  (MenuID)
+    CONSTRAINT MenuDetails_pk PRIMARY KEY (MenuID)
 );
 
-CREATE TABLE OrderDetails (
-    OrderID char(10)  NOT NULL,
-    ProductID char(10)  NOT NULL,
-    Quantity int  NOT NULL,
+CREATE TABLE OrderDetails
+(
+    OrderID   char(10) NOT NULL,
+    ProductID char(10) NOT NULL,
+    Quantity  int      NOT NULL,
     CONSTRAINT OrderID_OrderDetails_c CHECK (OrderID LIKE '^\d*$'),
     CONSTRAINT ProductID_OrderDetails_c CHECK (ProductID LIKE '^\d*$'),
     CONSTRAINT Quantity_OrderDetails_c CHECK (Quantity >= 0),
-    CONSTRAINT OrderDetails_pk PRIMARY KEY  (OrderID)
+    CONSTRAINT OrderDetails_pk PRIMARY KEY (OrderID)
 );
 
-CREATE TABLE Orders (
-    OrderID char(10)  NOT NULL,
-    CustomerID char(10)  NOT NULL,
-    OrderDate datetime  NOT NULL,
-    PaymentDate datetime  NULL,
-    PayVia char(10) NULL,
-    OrderStatus varchar(64)  NOT NULL,
-    RestaurantEmployeeID char(10)  NOT NULL,
+CREATE TABLE Orders
+(
+    OrderID              char(10)    NOT NULL,
+    CustomerID           char(10)    NOT NULL,
+    OrderDate            datetime    NOT NULL,
+    PaymentDate          datetime    NULL,
+    PayVia               char(10)    NULL,
+    OrderStatus          varchar(64) NOT NULL,
+    RestaurantEmployeeID char(10)    NOT NULL,
     CONSTRAINT OrderID_Orders_c CHECK (OrderID LIKE '^\d*$'),
     CONSTRAINT CustomerID_Orders_c CHECK (CustomerID LIKE '^\d*$'),
     CONSTRAINT PayVia_Orders_c CHECK (PayVia LIKE '^\d*$'),
     CONSTRAINT RestaurantEmployeeID_Orders_c CHECK (RestaurantEmployeeID LIKE '^\d*$'),
-    CONSTRAINT Orders_pk PRIMARY KEY  (OrderID)
+    CONSTRAINT Orders_pk PRIMARY KEY (OrderID)
 );
 
-CREATE TABLE PaymentMethod (
-    PaymentID char(10) NOT NULL,
+CREATE TABLE PaymentMethod
+(
+    PaymentID   char(10)    NOT NULL,
     PaymentName varchar(64) NOT NULL,
     CONSTRAINT PaymentID_PaymentMethod_c CHECK (PaymentID LIKE '^\d*$'),
-    CONSTRAINT PaymentMethod_pk PRIMARY KEY  (PaymentID)
+    CONSTRAINT PaymentMethod_pk PRIMARY KEY (PaymentID)
 )
 
-CREATE TABLE ProductIngredients (
-    ProductID char(10)  NOT NULL,
-    IngredientID char(10)  NOT NULL,
+CREATE TABLE ProductIngredients
+(
+    ProductID    char(10) NOT NULL,
+    IngredientID char(10) NOT NULL,
     CONSTRAINT ProductID_ProductIngredients_c CHECK (ProductID LIKE '^\d*$'),
     CONSTRAINT IngredientID_ProductIngredients_c CHECK (IngredientID LIKE '^\d*$'),
-    CONSTRAINT ProductIngredients_pk PRIMARY KEY  (ProductID)
+    CONSTRAINT ProductIngredients_pk PRIMARY KEY (ProductID)
 );
 
-CREATE TABLE IngredientsWarehouse (
-    IngredientID char(10)  NOT NULL,
-    IngredientName varchar(64)  NOT NULL,
-    QuantityLeft int  NOT NULL,
+CREATE TABLE IngredientsWarehouse
+(
+    IngredientID   char(10)    NOT NULL,
+    IngredientName varchar(64) NOT NULL,
+    QuantityLeft   int         NOT NULL,
     CONSTRAINT IngredientID_IngredientsWarehouse_c CHECK (IngredientID LIKE '^\d*$'),
     CONSTRAINT QuantityLeft_IngredientsWarehouse_c CHECK (QuantityLeft >= 0),
-    CONSTRAINT IngredientsWarehouse_pk PRIMARY KEY  (IngredientID)
+    CONSTRAINT IngredientsWarehouse_pk PRIMARY KEY (IngredientID)
 );
 
 
-CREATE TABLE Products (
-    ProductID char(10)  NOT NULL,
-    ProductName varchar(64)  NOT NULL,
-    CategoryID char(10)  NOT NULL,
-	CONSTRAINT ProductID_Products_c CHECK (ProductID LIKE '^\d*$'),
-    CONSTRAINT Products_pk PRIMARY KEY  (ProductID)
+CREATE TABLE Products
+(
+    ProductID   char(10)    NOT NULL,
+    ProductName varchar(64) NOT NULL,
+    CategoryID  char(10)    NOT NULL,
+    CONSTRAINT ProductID_Products_c CHECK (ProductID LIKE '^\d*$'),
+    CONSTRAINT Products_pk PRIMARY KEY (ProductID)
 );
 
-CREATE TABLE ProductPrices (
-    ProductID char(10)  NOT NULL,
-    FromTime datetime  NOT NULL,
-    ToTime datetime  NULL DEFAULT GETDATE(),
-    UnitPrice int  NOT NULL DEFAULT 0,
-	CONSTRAINT ProductID_ProductPrices_c CHECK (ProductID LIKE '^\d*$'),
-	CONSTRAINT Proper_Dates_ProductPrices_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
+CREATE TABLE ProductPrices
+(
+    ProductID char(10) NOT NULL,
+    FromTime  datetime NOT NULL,
+    ToTime    datetime NULL     DEFAULT GETDATE(),
+    UnitPrice int      NOT NULL DEFAULT 0,
+    CONSTRAINT ProductID_ProductPrices_c CHECK (ProductID LIKE '^\d*$'),
+    CONSTRAINT Proper_Dates_ProductPrices_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
     CONSTRAINT UnitPrice_ProductPrices_c CHECK (UnitPrice >= 0),
-    CONSTRAINT ProductPrices_pk PRIMARY KEY  (ProductID)
+    CONSTRAINT ProductPrices_pk PRIMARY KEY (ProductID)
 );
 
-CREATE TABLE Categories (
-    CategoryID char(10)  NOT NULL,
-    CategoryName varchar(64)  NOT NULL,
-	CONSTRAINT CategoryID_Categories_c CHECK (CategoryID LIKE '^\d*$'),
-    CONSTRAINT Categories_pk PRIMARY KEY  (CategoryID)
+CREATE TABLE Categories
+(
+    CategoryID   char(10)    NOT NULL,
+    CategoryName varchar(64) NOT NULL,
+    CONSTRAINT CategoryID_Categories_c CHECK (CategoryID LIKE '^\d*$'),
+    CONSTRAINT Categories_pk PRIMARY KEY (CategoryID)
 );
 
-CREATE TABLE Reservation (
+CREATE TABLE Reservation
+(
     ReservationID char(10) NOT NULL,
-    FromTime datetime  NOT NULL,
-    ToTime datetime  NOT NULL,
-    Seats int  NOT NULL,
-    DiningTableID char(10)  NOT NULL,
-    OrderID char(10)  NOT NULL,
-	CONSTRAINT ReservationID_Reservation_c CHECK (ReservationID LIKE '^\d*$'),
+    FromTime      datetime NOT NULL,
+    ToTime        datetime NOT NULL,
+    Seats         int      NOT NULL,
+    DiningTableID char(10) NOT NULL,
+    OrderID       char(10) NOT NULL,
+    CONSTRAINT ReservationID_Reservation_c CHECK (ReservationID LIKE '^\d*$'),
     CONSTRAINT DiningTableID_Reservation_c CHECK (DiningTableID LIKE '^\d*$'),
     CONSTRAINT OrderID_Reservation_c CHECK (OrderID LIKE '^\d*$'),
-	CONSTRAINT Seats_Reservation_c CHECK (Seats <= 20 AND Seats > 0),
-	CONSTRAINT Proper_Dates_Reservation_c CHECK (FromTime < ToTime),
-    CONSTRAINT Reservation_pk PRIMARY KEY  (ReservationID)
+    CONSTRAINT Seats_Reservation_c CHECK (Seats <= 20 AND Seats > 0),
+    CONSTRAINT Proper_Dates_Reservation_c CHECK (FromTime < ToTime),
+    CONSTRAINT Reservation_pk PRIMARY KEY (ReservationID)
 );
 
-CREATE TABLE RestaurantEmployees (
-    RestaurantEmployeeID char(10)  NOT NULL,
-    FirstName varchar(64)  NOT NULL,
-    LastName varchar(64)  NOT NULL,
-    Occupation varchar(64)  NOT NULL,
-    Street varchar(64)  NOT NULL,
-    Country varchar(64)  NOT NULL,
-    City varchar(64)  NOT NULL,
-    PostCode varchar(16)  NOT NULL,
-    Phone char(9)  NOT NULL,
-    Email varchar(64)  NOT NULL,
-	CONSTRAINT RestaurantEmployeeID_RestaurantEmployees_c CHECK (RestaurantEmployeeID LIKE '^\d*$'),
-	CONSTRAINT City_RestaurantEmployees_c CHECK ((City LIKE '^[A-Z]')),
-	CONSTRAINT Country_RestaurantEmployees_c CHECK ((Country LIKE '^[A-Z]')),
-	CONSTRAINT Name_Validation_RestaurantEmployees_c CHECK ((FirstName LIKE '^[A-Z][a-z]*$') AND (LastName LIKE '^[A-Z][a-z]*$')),
-	CONSTRAINT Phone_RestaurantEmployees_c CHECK (Phone LIKE '^[0-9]*$'),
-	CONSTRAINT Email_RestaurantEmployees_c CHECK ((Email LIKE '%@%.%')),
-    CONSTRAINT RestaurantEmployees_pk PRIMARY KEY  (RestaurantEmployeeID)
+CREATE TABLE RestaurantEmployees
+(
+    RestaurantEmployeeID char(10)    NOT NULL,
+    FirstName            varchar(64) NOT NULL,
+    LastName             varchar(64) NOT NULL,
+    Occupation           varchar(64) NOT NULL,
+    Street               varchar(64) NOT NULL,
+    Country              varchar(64) NOT NULL,
+    City                 varchar(64) NOT NULL,
+    PostCode             varchar(16) NOT NULL,
+    Phone                char(9)     NOT NULL,
+    Email                varchar(64) NOT NULL,
+    CONSTRAINT RestaurantEmployeeID_RestaurantEmployees_c CHECK (RestaurantEmployeeID LIKE '^\d*$'),
+    CONSTRAINT City_RestaurantEmployees_c CHECK ((City LIKE '^[A-Z]')),
+    CONSTRAINT Country_RestaurantEmployees_c CHECK ((Country LIKE '^[A-Z]')),
+    CONSTRAINT Name_Validation_RestaurantEmployees_c CHECK ((FirstName LIKE '^[A-Z][a-z]*$') AND (LastName LIKE '^[A-Z][a-z]*$')),
+    CONSTRAINT Phone_RestaurantEmployees_c CHECK (Phone LIKE '^[0-9]*$'),
+    CONSTRAINT Email_RestaurantEmployees_c CHECK ((Email LIKE '%@%.%')),
+    CONSTRAINT RestaurantEmployees_pk PRIMARY KEY (RestaurantEmployeeID)
 );
 
-CREATE TABLE EmployeesSalary (
-    RestaurantEmployeeID char(10)  NOT NULL,
-    FromTime datetime  NOT NULL,
-    ToTime datetime  NULL,
-    Salary int NOT NULL,
-	CONSTRAINT RestaurantEmployeeID_EmployeesSalary_c CHECK (RestaurantEmployeeID LIKE '^\d*$'),
-	CONSTRAINT Salary_EmployeesSalary_c CHECK (Salary >= 0),
-	CONSTRAINT Proper_Dates_EmployeesSalary_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
-    CONSTRAINT EmployeesSalary_pk PRIMARY KEY  (RestaurantEmployeeID)
+CREATE TABLE EmployeesSalary
+(
+    RestaurantEmployeeID char(10) NOT NULL,
+    FromTime             datetime NOT NULL,
+    ToTime               datetime NULL,
+    Salary               int      NOT NULL,
+    CONSTRAINT RestaurantEmployeeID_EmployeesSalary_c CHECK (RestaurantEmployeeID LIKE '^\d*$'),
+    CONSTRAINT Salary_EmployeesSalary_c CHECK (Salary >= 0),
+    CONSTRAINT Proper_Dates_EmployeesSalary_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
+    CONSTRAINT EmployeesSalary_pk PRIMARY KEY (RestaurantEmployeeID)
 );
 
-CREATE TABLE Takeaway (
-    OrderID char(10)  NOT NULL,
+CREATE TABLE Takeaway
+(
+    OrderID    char(10) NOT NULL,
     PickupDate datetime NULL,
-	CONSTRAINT orderID_Takeaway_c CHECK (OrderID LIKE '^\d*$'),
-    CONSTRAINT Takeaway_pk PRIMARY KEY  (OrderID)
+    CONSTRAINT orderID_Takeaway_c CHECK (OrderID LIKE '^\d*$'),
+    CONSTRAINT Takeaway_pk PRIMARY KEY (OrderID)
 );
 
-CREATE TABLE VariablesData (
-    FromTime datetime  NOT NULL,
-    ToTime datetime  NULL,
-    VariableType varchar(3)  NOT NULL,
-    VariableValue int  NOT NULL,
-	CONSTRAINT Proper_Dates_VariablesData_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
-	CONSTRAINT VariableValue_VariablesData_c CHECK (VariablesData.VariableValue >= 0)
+CREATE TABLE VariablesData
+(
+    FromTime      datetime   NOT NULL,
+    ToTime        datetime   NULL,
+    VariableType  varchar(3) NOT NULL,
+    VariableValue int        NOT NULL,
+    CONSTRAINT Proper_Dates_VariablesData_c CHECK (FromTime < ISNULL(ToTime, GETDATE())),
+    CONSTRAINT VariableValue_VariablesData_c CHECK (VariablesData.VariableValue >= 0)
 );
 
 
 -- Foreign Keys
-ALTER TABLE Products ADD CONSTRAINT Categories_Products
-    FOREIGN KEY (CategoryID)
-    REFERENCES Categories (CategoryID);
+ALTER TABLE Products
+    ADD CONSTRAINT Categories_Products
+        FOREIGN KEY (CategoryID)
+            REFERENCES Categories (CategoryID);
 
-ALTER TABLE Companies ADD CONSTRAINT Companies_Customers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE Companies
+    ADD CONSTRAINT Companies_Customers
+        FOREIGN KEY (CustomerID)
+            REFERENCES Customers (CustomerID);
 
-ALTER TABLE CompanyEmployees ADD CONSTRAINT CompanyEmployees_Companies
-    FOREIGN KEY (CompanyID)
-    REFERENCES Companies (CompanyID);
+ALTER TABLE CompanyEmployees
+    ADD CONSTRAINT CompanyEmployees_Companies
+        FOREIGN KEY (CompanyID)
+            REFERENCES Companies (CompanyID);
 
-ALTER TABLE EmployeesSalary ADD CONSTRAINT EmployeesSalary_RestaurantEmployee
-    FOREIGN KEY (RestaurantEmployeeID)
-    REFERENCES RestaurantEmployees (RestaurantEmployeeID);
+ALTER TABLE EmployeesSalary
+    ADD CONSTRAINT EmployeesSalary_RestaurantEmployee
+        FOREIGN KEY (RestaurantEmployeeID)
+            REFERENCES RestaurantEmployees (RestaurantEmployeeID);
 
-ALTER TABLE CompanyReservationParticipants ADD CONSTRAINT CompanyEmployees_CompanyReservationParticipants
-    FOREIGN KEY (CompanyEmployeeID)
-    REFERENCES CompanyEmployees (CompanyEmployeeID);
+ALTER TABLE CompanyReservationParticipants
+    ADD CONSTRAINT CompanyEmployees_CompanyReservationParticipants
+        FOREIGN KEY (CompanyEmployeeID)
+            REFERENCES CompanyEmployees (CompanyEmployeeID);
 
-ALTER TABLE CompanyReservationParticipants ADD CONSTRAINT CompanyReservationParticipants_Reservation
-    FOREIGN KEY (ReservationID)
-    REFERENCES Reservation (ReservationID);
+ALTER TABLE CompanyReservationParticipants
+    ADD CONSTRAINT CompanyReservationParticipants_Reservation
+        FOREIGN KEY (ReservationID)
+            REFERENCES Reservation (ReservationID);
 
-ALTER TABLE CompanyEmployees ADD CONSTRAINT CustomerPersonalData_CompanyEmployees
-    FOREIGN KEY (PersonID)
-    REFERENCES CustomersPersonalData (PersonID);
+ALTER TABLE CompanyEmployees
+    ADD CONSTRAINT CustomerPersonalData_CompanyEmployees
+        FOREIGN KEY (PersonID)
+            REFERENCES CustomersPersonalData (PersonID);
 
-ALTER TABLE IndividualCustomers ADD CONSTRAINT CustomerPersonalData_IndividualCustomers
-    FOREIGN KEY (PersonID)
-    REFERENCES CustomersPersonalData (PersonID);
+ALTER TABLE IndividualCustomers
+    ADD CONSTRAINT CustomerPersonalData_IndividualCustomers
+        FOREIGN KEY (PersonID)
+            REFERENCES CustomersPersonalData (PersonID);
 
-ALTER TABLE Orders ADD CONSTRAINT Customers_Orders
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE Orders
+    ADD CONSTRAINT Customers_Orders
+        FOREIGN KEY (CustomerID)
+            REFERENCES Customers (CustomerID);
 
-ALTER TABLE IndividualCustomers ADD CONSTRAINT IndividualCustomers_Customers
-    FOREIGN KEY (CustomerID)
-    REFERENCES Customers (CustomerID);
+ALTER TABLE IndividualCustomers
+    ADD CONSTRAINT IndividualCustomers_Customers
+        FOREIGN KEY (CustomerID)
+            REFERENCES Customers (CustomerID);
 
-ALTER TABLE ProductIngredients ADD CONSTRAINT IngredientsWarehouse_ProductIngredients
-    FOREIGN KEY (IngredientID)
-    REFERENCES IngredientsWarehouse (IngredientID);
+ALTER TABLE ProductIngredients
+    ADD CONSTRAINT IngredientsWarehouse_ProductIngredients
+        FOREIGN KEY (IngredientID)
+            REFERENCES IngredientsWarehouse (IngredientID);
 
-ALTER TABLE MenuDetails ADD CONSTRAINT MenuDetails_Products
-    FOREIGN KEY (ProductID)
-    REFERENCES Products (ProductID);
+ALTER TABLE MenuDetails
+    ADD CONSTRAINT MenuDetails_Products
+        FOREIGN KEY (ProductID)
+            REFERENCES Products (ProductID);
 
-ALTER TABLE MenuDetails ADD CONSTRAINT Menu_MenuDetails
-    FOREIGN KEY (MenuID)
-    REFERENCES Menu (MenuID);
+ALTER TABLE MenuDetails
+    ADD CONSTRAINT Menu_MenuDetails
+        FOREIGN KEY (MenuID)
+            REFERENCES Menu (MenuID);
 
-ALTER TABLE Invoices ADD CONSTRAINT Orders_Invoices
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE Invoices
+    ADD CONSTRAINT Orders_Invoices
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
 
-ALTER TABLE OrderDetails ADD CONSTRAINT Orders_OrderDetails
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE OrderDetails
+    ADD CONSTRAINT Orders_OrderDetails
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
 
-ALTER TABLE Reservation ADD CONSTRAINT Orders_Reservation
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE Reservation
+    ADD CONSTRAINT Orders_Reservation
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
 
-ALTER TABLE Orders ADD CONSTRAINT Orders_RestaurantEmployees
-    FOREIGN KEY (RestaurantEmployeeID)
-    REFERENCES RestaurantEmployees (RestaurantEmployeeID);
+ALTER TABLE Orders
+    ADD CONSTRAINT Orders_RestaurantEmployees
+        FOREIGN KEY (RestaurantEmployeeID)
+            REFERENCES RestaurantEmployees (RestaurantEmployeeID);
 
-ALTER TABLE Takeaway ADD CONSTRAINT Orders_Takeaway
-    FOREIGN KEY (OrderID)
-    REFERENCES Orders (OrderID);
+ALTER TABLE Takeaway
+    ADD CONSTRAINT Orders_Takeaway
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
 
-ALTER TABLE OrderDetails ADD CONSTRAINT Products_OrderDetails
-    FOREIGN KEY (ProductID)
-    REFERENCES Products (ProductID);
+ALTER TABLE OrderDetails
+    ADD CONSTRAINT Products_OrderDetails
+        FOREIGN KEY (ProductID)
+            REFERENCES Products (ProductID);
 
-ALTER TABLE ProductIngredients ADD CONSTRAINT Products_ProductIngredients
-    FOREIGN KEY (ProductID)
-    REFERENCES Products (ProductID);
+ALTER TABLE ProductIngredients
+    ADD CONSTRAINT Products_ProductIngredients
+        FOREIGN KEY (ProductID)
+            REFERENCES Products (ProductID);
 
-ALTER TABLE Reservation ADD CONSTRAINT Reservation_DiningTables
-    FOREIGN KEY (DiningTableID)
-    REFERENCES DiningTables (DiningTableID);
+ALTER TABLE Reservation
+    ADD CONSTRAINT Reservation_DiningTables
+        FOREIGN KEY (DiningTableID)
+            REFERENCES DiningTables (DiningTableID);
 
-ALTER TABLE ProductPrices ADD CONSTRAINT ProductPrices_Products
-    FOREIGN KEY (ProductID)
-    REFERENCES Products (ProductID);
+ALTER TABLE ProductPrices
+    ADD CONSTRAINT ProductPrices_Products
+        FOREIGN KEY (ProductID)
+            REFERENCES Products (ProductID);
 
-ALTER TABLE Orders ADD CONSTRAINT Orders_PaymentMethod
-    FOREIGN KEY (PayVia)
-    REFERENCES PaymentMethod (PaymentID);
+ALTER TABLE Orders
+    ADD CONSTRAINT Orders_PaymentMethod
+        FOREIGN KEY (PayVia)
+            REFERENCES PaymentMethod (PaymentID);
