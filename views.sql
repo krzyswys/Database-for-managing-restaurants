ed8a5be39498ce9e1fe47b4d6724db491735b8c5
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-11 23:16:40 +0100

    add current menu view

---
+++ views.sql
@@ -0,0 +1,12 @@
+--Views
+
+--Current menu view
+CREATE VIEW Current_Menu_View AS
+SELECT Products.ProductName, ProductPrices.UnitPrice
+FROM MenuDetails
+         JOIN Products ON Products.ProductID = MenuDetails.ProductID
+         JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
+WHERE MenuID = (SELECT MenuID
+                FROM dbo.Menu
+                WHERE ToTime IS NULL)
+AND ProductPrices.ToTime IS NULL
