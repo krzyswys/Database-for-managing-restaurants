f21719a993455fb64385c05eb63bb7eb6dfd7c56
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2023-01-06 19:17:23 +0100

    add new indexes

--- indexes.sql
+++ indexes.sql
@@ -1,1 +1,20 @@
+-- RestaurantEmployees_RestaurantEmployeeID_Index
+CREATE UNIQUE NONCLUSTERED INDEX RestaurantEmployees_RestaurantEmployeeID_Index ON RestaurantEmployees (RestaurantEmployeeID);
 
+-- Products_ProductID_Index
+CREATE UNIQUE NONCLUSTERED INDEX Products_ProductID_Index ON Products (ProductID);
+
+-- Menu_MenuID_Index
+CREATE UNIQUE NONCLUSTERED INDEX Menu_MenuID_Index ON Menu (MenuID);
+
+-- MenuDetails_MenuID_Index
+CREATE UNIQUE NONCLUSTERED INDEX MenuDetails_MenuID_Index ON MenuDetails (MenuID);
+
+-- Orders_OrderID_Index
+CREATE UNIQUE NONCLUSTERED INDEX Orders_OrderID_Index ON Orders (OrderID);
+
+-- OrderDetails_OrderID_Index
+CREATE UNIQUE NONCLUSTERED INDEX OrderDetails_OrderID_Index ON OrderDetails (OrderID);
+
+-- Customers_CustomerID_Index
+CREATE UNIQUE NONCLUSTERED INDEX Customers_CustomerID_Index ON Customers (CustomerID);
