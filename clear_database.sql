ca1db6dab5f07e3cbce6390a90e392aeb240e7aa
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2023-01-06 19:43:38 +0100

    Update clear_database.sql

--- clear_database.sql
+++ clear_database.sql
@@ -31,4 +31,21 @@ SELECT @sql = @sql + N' DROP FUNCTION '
                    + N'.' + QUOTENAME(name)
 FROM sys.objects
 WHERE type_desc LIKE '%FUNCTION%';
-Exec sp_executesql @sql
+EXEC sp_executesql @sql
+
+-- drop triggers
+SELECT @sql += 
+    N'DROP TRIGGER ' + 
+    QUOTENAME(OBJECT_SCHEMA_NAME(t.object_id)) + N'.' + 
+    QUOTENAME(t.name) + N'; ' + NCHAR(13)
+FROM sys.triggers AS t
+WHERE t.is_ms_shipped = 0
+ AND t.parent_class_desc = N'OBJECT_OR_COLUMN';
+
+-- drop indexes
+SELECT @sql = 
+(SELECT  'DROP INDEX ' + quotename(ix.name) + ' ON ' + quotename(object_schema_name(object_id)) + '.' + quotename(OBJECT_NAME(object_id)) + '; '
+FROM  sys.indexes ix
+WHERE   ix.Name IS NOT null and ix.Name like '%prefix_%'
+FOR XML PATH(''));
+EXEC sp_executesql @sql
