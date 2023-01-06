404771dc34d5946566d1d54e7d5d274abeccb0a8
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2023-01-06 19:44:27 +0100

    fix AddTableProcedure

--- create_database.sql
+++ create_database.sql
@@ -833,7 +833,7 @@ BEGIN
         DECLARE @DinigTableID INT
         SELECT @DinigTableID = ISNULL(MAX(DiningTables.DiningTableID), 0) + 1
         FROM DiningTables
-        INSERT INTO DiningTables(DiningTables.DiningTableID, NumberOfSeats)
+        INSERT INTO DiningTables(DiningTableID, NumberOfSeats)
         VALUES(@DinigTableID, @capacity);
     END TRY
     BEGIN CATCH
