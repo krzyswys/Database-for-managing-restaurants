74619cba3ceb265eadb1c356b030b0951b1a0896
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2023-01-06 19:18:32 +0100

    fix indexes

--- indexes.sql
+++ indexes.sql
@@ -8,13 +8,13 @@ CREATE UNIQUE NONCLUSTERED INDEX Products_ProductID_Index ON Products (ProductID
 CREATE UNIQUE NONCLUSTERED INDEX Menu_MenuID_Index ON Menu (MenuID);
 
 -- MenuDetails_MenuID_Index
-CREATE UNIQUE NONCLUSTERED INDEX MenuDetails_MenuID_Index ON MenuDetails (MenuID);
+CREATE NONCLUSTERED INDEX MenuDetails_MenuID_Index ON MenuDetails (MenuID);
 
 -- Orders_OrderID_Index
 CREATE UNIQUE NONCLUSTERED INDEX Orders_OrderID_Index ON Orders (OrderID);
 
 -- OrderDetails_OrderID_Index
-CREATE UNIQUE NONCLUSTERED INDEX OrderDetails_OrderID_Index ON OrderDetails (OrderID);
+CREATE NONCLUSTERED INDEX OrderDetails_OrderID_Index ON OrderDetails (OrderID);
 
 -- Customers_CustomerID_Index
 CREATE UNIQUE NONCLUSTERED INDEX Customers_CustomerID_Index ON Customers (CustomerID);
