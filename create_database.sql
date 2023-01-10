bc1743b44b6518cbaf19b3566c249b3c299de66c
Wiktor Wilkusz <76243064+Wajktor13@users.noreply.github.com>
2023-01-10 01:20:11 +0100

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -346,7 +346,6 @@ ALTER TABLE Orders
             REFERENCES Customers (CustomerID);
 GO
 
-
 -- VIEWS --
 CREATE VIEW Current_Menu_View AS
 SELECT Products.ProductID ,Products.ProductName, ProductPrices.UnitPrice
@@ -410,13 +409,6 @@ SELECT CustomerID, RestaurantEmployeeID, OrderID
 FROM Orders
 WHERE OrderStatus like '%not_confirmed%'
 GO
-
---Takaway_Orders_Pending_For_Pickup_View
---CREATE VIEW Takaway_Orders_Pending_For_Pickup_View AS
---SELECT CustomerID, RestaurantEmployeeID, OrderID
---FROM Orders
---WHERE Takeaway. is NULL --?TODO: create a takeaway_status?
---GO
 
 --Order_Details_View
 CREATE VIEW Order_Details_View
@@ -484,7 +476,7 @@ GO
 --Five_Best_Employees_View
 CREATE VIEW Five_Best_Employees_View AS
 SELECT TOP 5 RestaurantEmployees.RestaurantEmployeeID, FirstName,LastName FROM RestaurantEmployees
-INNER JOIN Orders ON Orders.RestaurantEmployeeID = RestaurantEmployees.RestaurantEmployeeID
+LEFT JOIN Orders ON Orders.RestaurantEmployeeID = RestaurantEmployees.RestaurantEmployeeID
 GROUP BY RestaurantEmployees.RestaurantEmployeeID, FirstName,LastName
 ORDER BY COUNT(OrderID) DESC
 GO
@@ -510,8 +502,8 @@ GO
 CREATE VIEW Available_Tables_View AS
 SELECT DISTINCT DiningTables.DiningTableID, DiningTables.NumberOfSeats
 FROM Reservation
-JOIN DiningTables ON DiningTables.DiningTableID = Reservation.DiningTableID
-WHERE ToTime < GETDATE()
+RIGHT JOIN DiningTables ON DiningTables.DiningTableID = Reservation.DiningTableID
+WHERE ToTime < GETDATE() OR ToTime IS NULL
 GO
 
 
@@ -581,296 +573,25 @@ GO
 -- OrderStatisticsView
 CREATE VIEW OrderStatisticsView
 AS
-SELECT 
+SELECT
 (SELECT COUNT(*) FROM Orders) as [całkowita liczba zamówień],
-(SELECT SUM(ProductPrices.UnitPrice*Quantity*(1- (DiscountPercent/100.0))) FROM Orders 
+(SELECT SUM(ProductPrices.UnitPrice*Quantity*(1- (DiscountPercent/100.0))) FROM Orders
 INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
 INNER JOIN Products ON OrderDetails.ProductID =  Products.ProductID
 INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-WHERE ProductPrices.FromTime < Orders.OrderDate AND (ProductPrices.ToTime = NULL OR ProductPrices.ToTime > Orders.OrderDate)
+WHERE ProductPrices.FromTime < Orders.OrderDate AND (ProductPrices.ToTime IS NULL OR ProductPrices.ToTime > Orders.OrderDate)
 ) as [całkowita cena zrealizowanych zamówień],
-(SELECT COUNT(*) FROM Orders 
-INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID 
+ (SELECT COUNT(*) FROM Orders
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
 INNER JOIN IndividualCustomers ON IndividualCustomers.CustomerID = Customers.CustomerID
 ) as [ilość zamowien dla klientow indywidualnych],
-(SELECT COUNT(*) FROM Orders 
-INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID 
+(SELECT COUNT(*) FROM Orders
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
 INNER JOIN Companies ON Companies.CustomerID = Customers.CustomerID
 ) as [ilość zamowien dla klientów firmowych],
-(SELECT COUNT(*) FROM Orders WHERE PaymentDate = NULL) as [ilość zamówień nieopłaconych],
-(SELECT COUNT(*) FROM Orders WHERE CollectDate = NULL) as [ilość zamówień nieodebranych],
+(SELECT COUNT(*) FROM Orders WHERE PaymentDate IS NULL) as [ilość zamówień nieopłaconych],
+(SELECT COUNT(*) FROM Orders WHERE CollectDate IS NULL) as [ilość zamówień nieodebranych],
 (SELECT TOP 1 OrderDate FROM Orders ORDER BY OrderDate DESC) as [data ostatnio zrealizowanego zamówienia]
-GO
-
-
-
--- FUNCTIONS --
-CREATE FUNCTION GetDetailsOfOrder(@input int)
-    RETURNS table AS
-        RETURN
-        SELECT OrderDetails.Quantity*ProductPrices.UnitPrice as Price, OrderDetails.Quantity*ProductPrices.UnitPrice*(1-Orders.DiscountPercent/100.0) as PriceAfterDiscount,
-	Orders.OrderID,Orders.OrderDate, Orders.OrderStatus, Orders.PayVia
-        FROM Orders
-        INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        WHERE Orders.OrderID = @input
-go
-
--- sprawdzenie stanu zamówienia po id
-CREATE FUNCTION GetStateOfOrder(@input int)
-    RETURNS table AS
-        RETURN
-        SELECT Orders.OrderStatus
-        FROM Orders
-        WHERE Orders.OrderID = @input
-go
-
-
--- wyposanie danych pracownika po id
-CREATE FUNCTION GetDataOfEmployeee(@input int)
-    RETURNS table AS
-        RETURN
-        SELECT RestaurantEmployees.FirstName as Imie, RestaurantEmployees.LastName as Nazwisko,  RestaurantEmployees.Phone as DaneKontaktowe, RestaurantEmployees.Occupation as Stanowisko, EmployeesSalary.FromTime as ZatrudnionyOd,  EmployeesSalary.ToTime as ZatrudnionyDo, EmployeesSalary.Salary
-        FROM RestaurantEmployees
-		INNER JOIN EmployeesSalary ON EmployeesSalary.RestaurantEmployeeID = RestaurantEmployees.RestaurantEmployeeID
-        WHERE RestaurantEmployees.RestaurantEmployeeID = @input
-go
-
-
--- zamówienia powyżej jakiejś wartości
-CREATE FUNCTION GetOrdersAboveValue(@input int)
-    RETURNS table AS
-        RETURN
-        SELECT Orders.OrderID, Orders.OrderStatus, OrderDetails.Quantity*ProductPrices.UnitPrice*(1-Orders.DiscountPercent/100.0) AS PriceAfterDiscount
-        FROM Orders
-        INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        WHERE OrderDetails.Quantity*ProductPrices.UnitPrice > @input
-go
-
--- suma zamówień danego dnia
-CREATE FUNCTION GetValueOfOrdersOnDay(@date date)
-    RETURNS table AS
-        RETURN  
-        SELECT SUM(OrderDetails.Quantity*ProductPrices.UnitPrice*(1-Orders.DiscountPercent/100.0)) as Suma
-        FROM Orders
-        INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        WHERE YEAR(@date) = YEAR(Orders.OrderDate)
-        AND MONTH(@date) = MONTH(Orders.OrderDate)
-        AND DAY(@date) = DAY(Orders.OrderDate)
-go
-
--- suma zamówień danego miesiąca
-CREATE FUNCTION GetValueOfOrdersOnMonth(@date date)
-    RETURNS table AS
-        RETURN
-	SELECT SUM(OrderDetails.Quantity*ProductPrices.UnitPrice*(1-Orders.DiscountPercent/100.0)) as Suma
-        FROM Orders
-        INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        WHERE YEAR(@date) = YEAR(Orders.OrderDate)
-        AND MONTH(@date) = MONTH(Orders.OrderDate)
-go
-
--- wartosc X zamówienia
-CREATE FUNCTION GetValueOfOrder(@input int)
-    RETURNS int AS
-	BEGIN
-	DECLARE @value INT;
-        SELECT   @value = OrderDetails.Quantity*ProductPrices.UnitPrice*(1-Orders.DiscountPercent/100.0)
-        FROM Orders
-        INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        WHERE (Orders.OrderID = @input) AND (Orders.OrderDate >= ProductPrices.FromTime AND ( ProductPrices.ToTime is NULL OR ProductPrices.ToTime>=Orders.OrderDate))
-	RETURN IsNull(@value, 0);
-	END
-go
-
--- wartosc najtańszy produkt w kategorii
-CREATE FUNCTION GetCheapestProductInCategory(@input int)
-    RETURNS table AS
-        RETURN
-        SELECT TOP(1) ProductPrices.UnitPrice as cena, ProductName
-        FROM Products
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        INNER JOIN Categories ON Categories.CategoryID = Products.CategoryID
-        WHERE Categories.CategoryID = @input
-        ORDER BY cena DESC
-go
-
--- wartosc najdroższy produkt w kategorii
-CREATE FUNCTION GetMostExpensiveProductInCategory(@input char)
-    RETURNS table AS
-        RETURN
-        SELECT TOP(1) ProductPrices.UnitPrice as cena, ProductName
-        FROM Products
-        INNER JOIN ProductPrices ON ProductPrices.ProductID = Products.ProductID
-        INNER JOIN Categories ON Categories.CategoryID = Products.CategoryID
-        WHERE Categories.CategoryID = @input
-go
-
-
--- GetIngredientsForProduct
-CREATE FUNCTION GetIngredientsForProduct(@ProductID int)
-    RETURNS table
-	AS
-	RETURN
-		SELECT IngredientName
-		FROM ProductIngredients
-		JOIN IngredientsWarehouse ON IngredientsWarehouse.IngredientID = ProductIngredients.IngredientID
-		WHERE ProductIngredients.ProductID = @ProductID
-GO
-
--- GetProductsFromCategory
-CREATE FUNCTION GetProductsFromCategory(@CategoryID int)
-    RETURNS table
-	AS
-	RETURN
-		SELECT Products.ProductName
-		FROM Products
-		JOIN Categories ON Categories.CategoryID = Products.CategoryID
-		WHERE Categories.CategoryID = @CategoryID
-GO
-
--- GetProductsFromMenu
-CREATE FUNCTION GetProductsFromMenu(@MenuID int)
-    RETURNS table
-	AS
-	RETURN
-		SELECT Products.ProductName
-		FROM Menu
-		JOIN MenuDetails ON MenuDetails.MenuID = Menu.MenuID
-		JOIN Products ON Products.ProductID = MenuDetails.ProductID
-		WHERE Menu.MenuID = @MenuID
-GO
-
--- GetCurrentEmployeeSalary
-CREATE FUNCTION GetCurrentEmployeeSalary(@EmployeeID int)
-RETURNS table
-AS
-RETURN
-   SELECT Salary
-   FROM EmployeesSalary
-   WHERE RestaurantEmployeeID = @EmployeeID
-   AND ToTime IS NULL
-GO
-
--- GetCurrentAverageSalaryForOccupation
-CREATE FUNCTION GetCurrentAverageSalaryForOccupation(@Occupation varchar(64))
-RETURNS table
-AS
-RETURN
-   SELECT AVG(Salary) AS AveragePriceForOccupation
-   FROM EmployeesSalary
-   JOIN RestaurantEmployees ON EmployeesSalary.RestaurantEmployeeID = RestaurantEmployees.RestaurantEmployeeID
-   WHERE Occupation = @Occupation
-   AND ToTime IS NULL
-GO
-
--- GetTotalProductsAndAveragePriceOfMenu
-CREATE FUNCTION GetTotalProductsAndAveragePriceOfMenu(@MenuID int)
-RETURNS table
-AS
-RETURN
-   SELECT COUNT(UnitPrice) AS TotalProducts, AVG(UnitPrice) AS AverageUnitPrice
-   FROM Menu
-   JOIN MenuDetails ON Menu.MenuID = MenuDetails.MenuID
-   JOIN ProductPrices ON MenuDetails.ProductID = ProductPrices.ProductID
-   WHERE Menu.MenuID = @MenuID
-   AND ProductPrices.ToTime IS NULL
-GO
-
--- GetHighestSalaryForEmployee
-CREATE FUNCTION GetHighestSalaryForEmployee(@EmployeeID int)
-RETURNS table
-AS
-RETURN
-   SELECT MAX(Salary) AS MaxEmployeeSalary
-   FROM EmployeesSalary
-   WHERE RestaurantEmployeeID = @EmployeeID
-GO
-
---Sprawdzenie ile dni pozostało na dane menu - pracownik
-CREATE FUNCTION RemainingDaysForMenu(@MenuID int)
-RETURNS table
-AS
-RETURN 
-	SELECT DATEDIFF(day, GETDATE(), (SELECT 
-		ToTime 
-		FROM Menu 
-		WHERE MenuID = @MenuID )) as [remaining days]
-GO
-
-
-CREATE FUNCTION RemainingFreeSeats()
-RETURNS table
-AS 
-RETURN 
-	SELECT (
-	(SELECT 
-	SUM(NumberOfSeats) 
-	FROM DiningTables) - 
-	
-	(SELECT 
-	SUM(Seats)
-	FROM Reservation 
-	WHERE GETDATE() >= FromTime AND GETDATE() <= ToTime
-	)) as [free seats]
-GO
-
-CREATE FUNCTION CanAccommodateCustomers(@customers int)
-RETURNS table
-AS
-RETURN
-	SELECT  (CASE WHEN free_seats.nrOfFreeSeats >= @customers THEN 'true' ELSE 'false' END) AS freeSpaces
-	FROM (SELECT ((SELECT SUM(NumberOfSeats) FROM DiningTables) - (SELECT SUM(Seats) FROM Reservation WHERE GETDATE() >= FromTime AND GETDATE() <= ToTime)) as nrOfFreeSeats
-	) as free_seats 
-GO
-
-
-CREATE FUNCTION invoice(@ordersID int)
-RETURNS table
-AS
-RETURN
-       SELECT Products.ProductName as[nazwa produktu], 
-        OrderDetails.Quantity as [ilosc], 
-        ProductPrices.UnitPrice as [cena produktu], 
-        Orders.DiscountPercent as [zniżka], 
-        ProductPrices.UnitPrice*(1- (Orders.DiscountPercent/100.0)) as [cena produktu z uwzględnieniem zniżki],
-		Orders.OrderDate as [data zamowienia] FROM Orders 
-        INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON Products.ProductID = ProductPrices.ProductID
-        WHERE ProductPrices.FromTime >= Orders.OrderDate AND (ProductPrices.ToTime = NULL OR ProductPrices.ToTime  >= Orders.OrderDate)
-        AND Orders.OrderID = @ordersID
-GO
-
-CREATE FUNCTION collectiveInvoice(@companyID int)
-RETURNS table
-AS 
-RETURN 
-        SELECT 
-        SUM(OrderDetails.Quantity*ProductPrices.UnitPrice*(1-(Orders.DiscountPercent/100.0))) as [calkowita cena produktow],
-        ROUND(AVG(Orders.DiscountPercent),2) as [srednia znizek],
-        Orders.OrderDate as [data zamowienia],
-        Companies.NIP as [NIP firmy]
-        FROM Companies 
-        INNER JOIN  Customers ON Companies.CustomerID = Customers.CustomerID
-        INNER JOIN ORDERS ON Customers.CustomerID = Orders.CustomerID
-        INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
-        INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-        INNER JOIN ProductPrices ON Products.ProductID = ProductPrices.ProductID
-        WHERE MONTH(Orders.OrderDate) = MONTH(GETDATE()) 
-        AND YEAR(Orders.OrderDate) = YEAR(GETDATE())
-        AND Companies.CompanyID = @companyID 
-        AND ProductPrices.FromTime >= Orders.OrderDate AND (ProductPrices.ToTime = NULL OR ProductPrices.ToTime  >= Orders.OrderDate)
-        GROUP BY Orders.OrderDate,Companies.NIP
 GO
 
 
@@ -959,7 +680,7 @@ BEGIN
         )
         BEGIN
             ;
-            THROW 52000, 'Brak kategorii', 1 --można by jakoś wtedy dodać kategorie razem z produktem jak rzuci taki błąd, ale wtedy np: 'ser', 'nabial' utworzy nową kategoria zamiast wpaść do 'nabiał'
+            THROW 52000, 'Brak kategorii', 1
         END
     DECLARE @CategoryID INT
     SELECT @CategoryID = CategoryID
